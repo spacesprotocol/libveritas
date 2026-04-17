68611b6d7a0e4341872b8fd4b8c7da6f0631a0e1
Buffrr <contact@buffrr.dev>
2026-04-17 18:45:58 +0200

    ci: skip rustfmt on autogenerated guest image ID arrays

--- veritas/src/constants.rs
+++ veritas/src/constants.rs
@@ -5,7 +5,9 @@
 // To update after changing guest programs, run:
 //   ./update-elfs.sh
 
+#[rustfmt::skip]
 pub const FOLD_ID: [u32; 8] = [3538164873, 3494660837, 1605885420, 2756930862, 1952720968, 91802116, 3635727049, 436347682];
+#[rustfmt::skip]
 pub const STEP_ID: [u32; 8] = [2719979593, 62333512, 1158600685, 3512173834, 1442236244, 869560259, 553115519, 3467999922];
 
 #[cfg(feature = "elf")]
